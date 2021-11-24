9f90c97bb89e308a39bc6af5b0419ecbe5f88807
tjsingh <taranjeets120@gmail.com>
2021-11-25 00:28:05 +0530

    created room context

---
+++ src/Context/rooms.context.js
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { database } from '../misc/firebase';
+import { transformToArrWithId } from '../misc/helpers';
+
+const RoomContext = createContext();
+
+
+export const RoomProvider = ({ children }) => {
+
+    const [room, setroom] = useState(null)
+
+    useEffect(() => {
+        const roomListRef = database.ref('rooms');
+
+        roomListRef.on('value', (snap) => {
+            const data = transformToArrWithId(snap.val());
+            setroom(data)
+        })
+
+        return () => {
+            roomListRef.off();
+        }
+    }, [])
+
+    return <RoomContext.Provider value={room}>{children}</RoomContext.Provider>
+}
